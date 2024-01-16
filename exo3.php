9ccdc4a0522a43606121a56c7e726616dc0a8fc2
saidtimaev <said.timaev@stagiaire-elan.fr>
2024-01-16 21:14:09 +0100

    MAJ EXO3 MAISON

--- exo3.php
+++ exo3.php
@@ -1,13 +1,19 @@
 <h1>Exercice 3</h1>
 
 <p>A partir de la phrase de l’exercice 1, écrire l’instruction permettant de remplacer le mot 
-« aujourd’hui » par le mot « demain ». Afficher l’ancienne et la nouvelle phrase.
+« aujourd’hui » par le mot « demain ».<br> Afficher l’ancienne et la nouvelle phrase.
 </p> 
 
 <h2>Résultat</h2>
 
+
+
 <?php
-$phrase = "Notre formation DL commence aujourd'hui";
-$nbMots = str_word_count($phrase);
-echo "La phrase « Notre formation DL commence aujourd'hui » contient $nbMots mots.";
+  $phrase1 = "Notre formation DL commence aujourd’hui<br>";
+  $phrase2 = "Notre formation DL commence aujourd’hui";
+
+  echo $phrase1;
+  echo str_replace("aujourd'hui", "demain", $phrase2);
 ?>
+
+
